08ec7a8793918af0bdef9e2a2dddd9e6863f431c
forgacs.daniel <forgacs.daniel@gmail.com>
2016-01-15 23:08:04 +0000

    bakeparm test update

--- python/bakeparm_test.py
+++ python/bakeparm_test.py
@@ -1,2 +1,34 @@
+"""
+"""
+
+
+import unittest
+# import sys
+# import os
+
+
+# modulepath = os.path.dirname(__file__)
+# # houpath = 'C:/Apps/HOUDIN~1.313/houdini/python2.7libs\hou.pyc'
+# houpath = 'c:/Apps/HOUDIN~1.313/houdini/python2.7libs'
+# sys.path.insert(0, houpath)
+
+# if modulepath not in sys.path:
+#     sys.path.insert(0, modulepath)
+
+
+# import hou
+# # import bakeparm
+# # from bakeparm import bake_parm
+
+
+
+class BakeParmTests(unittest.TestCase):
+    def setUp(self):
+        geo = hou.node('/obj').createNode('geo')
+
+    def tearDown(self):
+        pass
+
+
 def main():
     pass
