7dd62d7bcb23c814ec435538213dde2b86d0d22d
forgacs.daniel <forgacs.daniel@gmail.com>
2016-01-16 17:42:43 +0000

    refactor cache setup tests

--- python/setupcache_test.py
+++ python/setupcache_test.py
@@ -10,37 +10,10 @@ reload(setupcache)
 
 
 
-class SetupCacheTests(unittest.TestCase):
+class HipTest(unittest.TestCase):
     """
-    unit tests
-    """
-
-    def setUp(self):
-        hou.hipFile.clear(suppress_save_prompt=True)
-
-        geo = hou.node('/obj').createNode('geo', 'TEST_geo')
-        geo.node('file1').destroy()
-
-        name = ''. join([random.choice('qwertyuiop') for k in range(10)])
-        box = geo.createNode('box', name)
-        box.setSelected(True)
-        box.setCurrent(True)
-
-    def tearDown(self):
-        pass
-
-    def test__get_sop_from_selection__returns_sop(self):
-        node = setupcache.get_sop_from_selection()
-
-        self.assertTrue(type(node) is hou.SopNode)
-        self.assertTrue(type(node.type()) is hou.SopNodeType)
-        self.assertTrue(isinstance(node.type(), hou.SopNodeType))
-        self.assertIsInstance(node.type(), hou.SopNodeType)
-
-
-class SetupCacheFunctonalTests(unittest.TestCase):
-    """
-    functional tests
+    base class to initiate houdini scene
+    across tests
     """
 
     def setUp(self):
@@ -55,6 +28,27 @@ class SetupCacheFunctonalTests(unittest.TestCase):
         self.box.createOutputNode('smooth')
         self.box.setSelected(True)
         self.box.setCurrent(True)
+
+
+
+class SetupCacheTests(HipTest):
+    """
+    unit tests
+    """
+
+    def test__get_sop_from_selection__returns_sop(self):
+        node = setupcache.get_sop_from_selection()
+
+        self.assertTrue(type(node) is hou.SopNode)
+        self.assertTrue(type(node.type()) is hou.SopNodeType)
+        self.assertTrue(isinstance(node.type(), hou.SopNodeType))
+        self.assertIsInstance(node.type(), hou.SopNodeType)
+
+
+class SetupCacheFunctonalTests(HipTest):
+    """
+    functional tests
+    """
 
     def test_setup_cache(self):
         setupcache.main2()
